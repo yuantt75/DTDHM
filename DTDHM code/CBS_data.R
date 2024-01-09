220c8ea78fd6d4819dafc1967e101795695a3bc7
yuantt75 <1057250798@qq.com>
2024-01-09 11:05:01 +0800

    first commit

---
+++ DTDHM code/CBS_data.R
@@ -0,0 +1,62 @@
+library(DNAcopy)
+"CBS_data"<-function(){
+  data = read.table("/media/yuantt/USB移动磁盘/totalModel/run_KNNTD+CBS+dr3+MQ/RD")
+  #data = data[-1, -(1:2)]
+  #data = log2(data/2)
+  # matrix()创建矩阵 行为data个数 列为3 数据填充0
+  # nrow：矩阵的行数 ncol：矩阵的列数
+  head = matrix(0, nrow(data), 3)
+  head[,1] = 1
+  head[,2] = 1:nrow(data)
+  head[,3] = 1:nrow(data)
+  
+  chrom <- rep(1, nrow(data))
+  maploc <- 1:nrow(data)
+  seg.file_g = matrix(0,1,6)
+  seg.file_g_one = matrix(0,1,6)
+  seg.file = matrix(0, nrow(data), 1)
+  
+  stac_amp = matrix(0, 1, nrow(data))
+  stac_amp[1,] = 1:nrow(data)
+  stac_amp_one = matrix(0, 1, nrow(data))
+  
+  stac_del = matrix(0, 1, nrow(data))
+  stac_del[1,] = 1:nrow(data)
+  stac_del_one = matrix(0, 1, nrow(data))
+  
+  
+  for (j in 1:ncol(data)){
+    
+    #cat("sampl No,",j,"\n")
+    #data <- CNA(data[,j],chrom,maploc)
+    seg<- segment(CNA(data[,j],chrom,maploc))
+    for (k in 1:length(seg$output$loc.start)){
+      seg.file_g_one[1,1]=j
+      seg.file_g_one[1,2]=1
+      seg.file_g_one[1,3]=seg$output$loc.start[k]
+      seg.file_g_one[1,4]=seg$output$loc.end[k]
+      seg.file_g_one[1,5]=seg$output$num.mark[k]
+      seg.file_g_one[1,6]=seg$output$seg.mean[k]
+      seg.file_g=rbind(seg.file_g,seg.file_g_one)
+      seg.file_g_one=matrix(0,1,6)
+      
+    }
+    
+    
+  }
+  seg.file_g = seg.file_g[-1,]
+  
+  out.file=getwd()
+  out.file=paste(out.file,'seg',sep="/")
+
+  write.table(seg.file_g,file=out.file,row.names=F,col.names=F,quote=F,sep="\t")
+  
+}
+CBS_data()
+
+
+
+
+  
+
+
